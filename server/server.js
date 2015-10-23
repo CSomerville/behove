f4d6c085c11d00f96770083ed7d751648dc1612b
Colby Somerville <colby.somerville@gmail.com>
2015-10-22 20:27:13 -0400

    deleting server cruft:

--- server/server.js
+++ server/server.js
@@ -26,58 +26,6 @@ app.use(session({
 
 app.use('/', routes);
 
-// function restrict(req, res, next) {
-//   if (req.session.user) {
-//     next();
-//   } else {
-//     req.session.error = 'Access Denied';
-//     res.redirect('/login');
-//   }
-// }
-
-// app.get('/login', (req, res) => {
-//   res.send('<form action="/login" method="POST">' +
-//     '<input type="text" name="username"><button type="submit">ung</button></form>' +
-//     '<form action="/adduser" method="POST">' +
-//     '<input type="text" name="username"><button type="submit">dork</button></form>')
-// });
-//
-// app.get('/logout', (req, res) => {
-//   req.session.destroy(() => {
-//     res.redirect('/login');
-//   });
-// });
-//
-// app.get('/loggedin', restrict, (req, res) => {
-//   res.send('hi');
-// });
-//
-// app.post('/login', (req, res) => {
-//   db.task((t) => {
-//     return t.one("SELECT * FROM users WHERE username='" + req.body.username + "';");
-//   })
-//     .then((data) => {
-//       console.log(data);
-//       req.session.regenerate(() => {
-//         req.session.user = data.id;
-//         res.redirect('/loggedin');
-//       });
-//     }, (err) => {
-//       console.log(err);
-//       res.redirect('/login');
-//     });
-// });
-//
-// app.post('/adduser', (req, res) => {
-//   createUser(req.body.username)
-//     .then(() => {
-//       res.redirect('/loggedin');
-//     }, (err) => {
-//       res.redirect('/login');
-//     })
-// });
-
-
 app.listen(app.get('port'), () => {
   console.log('server is listening on ', app.get('port'));
 });
