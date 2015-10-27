ff861c1eb9fa5abb91a002f39c82e08545dec32a
Colby Somerville <colby.somerville@gmail.com>
2015-10-26 22:39:49 -0400

    initiate post comb fn

--- assets/actions/index.js
+++ assets/actions/index.js
@@ -1,9 +1,40 @@
 import fetch from 'isomorphic-fetch';
 
+export const NEW_COMB = 'NEW_COMB';
+export const CANCEL_NEW_COMB = 'CANCEL_NEW_COMB';
 export const POST_COMB = 'POST_COMB';
 export const POST_COMB_SUCCESS = 'POST_COMB_SUCCESS';
 export const POST_COMB_FAILURE = 'POST_COMB_FAILURE';
 
+export function newComb() {
+  return { type: 'NEW_COMB' };
+}
+
+export function cancelNewComb() {
+  return { type: 'CANCEL_NEW_COMB' };
+}
+
+function postComb(comb) {
+  return {
+    type: 'POST_COMB',
+    comb
+  }
+}
+
+function postCombSuccess(comb) {
+  return {
+    type: 'POST_COMB_SUCCESS',
+    comb
+  }
+}
+
+function postCombFailure(comb, err) {
+  return {
+    type: 'POST_COMB_FAILURE',
+    comb,
+    err
+  }
+}
 
 // export const INCREMENT = 'INCREMENT';
 // export const DECREMENT = 'DECREMENT';
